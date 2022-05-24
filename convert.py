6bf9bbb2b9a4d832d1208b65a03b1b20381116ae
tPhophan <lamyong258@gmail.com>
2022-05-24 13:27:41 +0700

    24052022

--- convert.py
+++ convert.py
@@ -4,8 +4,8 @@ f = open("rates.json")
 f2 = open("dropdown.txt", "w")
 data  = json.load(f)
 for i in data['conversion_rates']:
-    #<option value="AED"</option>
-    f2.write(f'<option value=\"{i}\"</option>\n')
+    #<option value="AED">AED</option>
+    f2.write(f'<option value=\"{i}\">{i}</option>\n')
     
 
 f.close()
